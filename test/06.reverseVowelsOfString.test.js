1901658837b919b19efa64a58330ec6e56692acd
soozynn <junsuejin98@gmail.com>
2024-07-09 13:40:42 +0900

    Unsolve: 06

--- test/06.reverseVowelsOfString.test.js
+++ test/06.reverseVowelsOfString.test.js
@@ -1,1 +1,11 @@
 // TODO: 문제 예시를 보고 직접 테스트 케이스를 작성해주세요.
+
+import { expect } from "chai";
+import reverseVowelsOfString from "../lib/06.reverseVowelsOfString.js";
+
+describe("should pass all cases", function () {
+  it("should pass base cases", function () {
+    expect(reverseVowelsOfString("hello")).to.eql("holle");
+    expect(reverseVowelsOfString("leetcode").to.eql("leotcede"));
+  });
+});
